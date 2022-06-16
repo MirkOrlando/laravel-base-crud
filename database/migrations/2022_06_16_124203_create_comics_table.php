2abe87ec1bfe0158861bb50b6c4433f8868fe51d
MirkOrlando <mirko@orlando-m.it>
2022-06-16 15:12:26 +0200

    implemente ComicSeed

--- database/migrations/2022_06_16_124203_create_comics_table.php
+++ database/migrations/2022_06_16_124203_create_comics_table.php
@@ -15,6 +15,13 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50);
+            $table->text('description')->nullable();
+            $table->string('thumb')->nullable();
+            $table->decimal('price', 5, 2, true)->nullable();
+            $table->string('series', 50)->nullable();
+            $table->date('sale_date')->nullable();
+            $table->string('type', 20)->nullable();
             $table->timestamps();
         });
     }
